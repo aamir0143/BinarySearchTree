01082b5ec289acff0d318125a3442f9f2023358c
aamir0143 <mdaamirreza00@gmail.com>
2022-02-06 21:01:57 +0530

    [Aamir15] Add : Create a BST by Adding

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("==========Welcome To Binary Search Tree Problems==========");
-Console.ReadLine();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Displaying the message
+            Console.WriteLine("==========Welcome To Binary Search Tree Problems==========");
+            Console.WriteLine();
+            //Creating binary search tree to insert and display node values(UC1)
+            BinSearchTree<int> binaryTree = new BinSearchTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
+            Console.WriteLine("\n===================================================================");
+            Console.ReadLine();
+        }
+    }
+}
